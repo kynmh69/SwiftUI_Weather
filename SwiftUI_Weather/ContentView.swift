c042d153e152327af43494af5de92bb58a1c44ab
kynmh69 <hiroki.kayanuma@gmail.com>
2021-04-29 11:45:20 +0900

    add view

--- SwiftUI_Weather/ContentView.swift
+++ SwiftUI_Weather/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(content: {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading
+                           , endPoint:.bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack(content: {
+                Text("Cuptino, CA")
+                    .font(.system(size: 32,weight: .medium,design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing:10, content: {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("76°")
+                        .font(.system(size: 79,weight: .medium))
+                        .foregroundColor(.white)
+                })
+                HStack(content: {
+                    VStack(content: {
+                        Text("TUE")
+                            .font(.system(size: 16, weight: .medium,design: .default))
+                            .foregroundColor(.white)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:40, height: 40)
+                        Text("76°")
+                            .font(.system(size: 28,weight: .medium))
+                            .foregroundColor(.white)
+                    })
+                })
+                Spacer()
+            })
+        })
     }
 }
 
