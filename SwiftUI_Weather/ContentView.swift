f21ed3800465d109231410eb736541ba715c9107
kynmh69 <hiroki.kayanuma@gmail.com>
2021-04-29 11:11:04 +0900

    Initial Commit

---
+++ SwiftUI_Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Weather
+//
+//  Created by Hiroki Kayanuma on 2021/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI_Weather
+//
+//  Created by Hiroki Kayanuma on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
